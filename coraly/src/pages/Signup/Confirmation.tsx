4c7d1d66da13089abd4ba50c588d59ed2ba28b95
nemanja <dobranicnemanja2014@gmail.com>
2022-11-02 15:03:17 +0100

    Signup email sent & step 2 template completed

--- coraly/src/pages/Signup/Confirmation.tsx
+++ coraly/src/pages/Signup/Confirmation.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import GetStarted from "../../layouts/GetStarted";
+import { Box, Typography } from "@mui/material";
+import { theme } from "../../config/theme";
+import Done from "../../assets/images/done.svg";
 
 function Confirmation() {
   return (
-    <div>Confirmation</div>
-  )
+    <GetStarted>
+      <Typography variant="h4" color={theme.palette.grey[900]} marginBottom={1}>
+        Your workspace is ready
+      </Typography>
+      <Typography
+        variant="body1"
+        color={theme.palette.grey[700]}
+        marginBottom={7}
+      >
+        Check your email inbox. <br />
+        We sent you a confirmation email.
+      </Typography>
+      <Box
+        component="img"
+        alt="logo"
+        src={Done}
+        sx={{
+          left: "-2.5%",
+          position: "relative",
+          [theme.breakpoints.up(0)]: {
+            maxWidth: "34px",
+          },
+          [theme.breakpoints.up("md")]: {
+            maxWidth: "54px",
+          },
+          [theme.breakpoints.up("lg")]: {
+            maxWidth: "72px",
+          },
+          [theme.breakpoints.up("xl")]: {
+            maxWidth: "100px",
+          },
+        }}
+      ></Box>
+    </GetStarted>
+  );
 }
 
-export default Confirmation
+export default Confirmation;
--- coraly/src/pages/Signup/Profile.tsx
+++ coraly/src/pages/Signup/Profile.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import GetStarted from "../../layouts/GetStarted";
+import { Typography, TextField, Button } from "@mui/material";
+import { theme } from "../../config/theme";
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <GetStarted>
+      <Typography variant="h4" color={theme.palette.grey[900]} marginBottom={1}>
+        Complete your profile
+      </Typography>
+      <Typography
+        variant="body1"
+        color={theme.palette.grey[700]}
+        marginBottom={6.25}
+      >
+        Insert all your info to proceed with your workspace
+      </Typography>
+
+      <form>
+        <TextField
+          label="Name"
+          variant="outlined"
+          type="text"
+          sx={{ marginBottom: "24px", width: "48%" }}
+        />
+        <TextField
+          label="Surname"
+          variant="outlined"
+          type="text"
+          fullWidth
+          sx={{ marginBottom: "24px", width: "48%", float: "right" }}
+        />
+
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          fullWidth
+          sx={{ marginBottom: "16px" }}
+        />
+
+        <TextField
+          label="Repeat Password"
+          variant="outlined"
+          type="password"
+          fullWidth
+          sx={{ marginBottom: "32px" }}
+        />
+
+        <Button
+          variant="contained"
+          sx={{ marginBottom: "2rem" }}
+          color="secondary"
+          fullWidth
+        >
+          <Typography variant="button">Complete now</Typography>
+        </Button>
+      </form>
+    </GetStarted>
+  );
 }
 
-export default Profile
+export default Profile;
