1e0ae38a42a3cbe4fdb039b6130c80ea81a401b3
Nemanja Dobranic <dobranicnemanja2014@gmail.com@>
2022-11-12 13:11:07 +0100

    fixed signup guard

--- coraly/src/components/guards/SignupGuard.tsx
+++ coraly/src/components/guards/SignupGuard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router";
 import { IRootState } from "../../redux/rootReducer";
@@ -6,6 +6,8 @@ import { useDispatch } from "react-redux";
 import { createWorkspace } from "../../redux";
 import { signupKey } from "../../config/localStorageKeys";
 import useLocalStorage from "../../hooks/useLocalStorage";
+import NotFound from "../../pages/NotFound";
+import { confirmSignupEmail } from "../../redux";
 
 export enum SignupEnum {
   WORKSPACE = "WORKSPACE",
@@ -22,28 +24,40 @@ const SignupGuard: React.FC<IPros> = ({ type, children }) => {
   const { workspace, email, agreed, authorized, confirmed } = useSelector(
     (state: IRootState) => state.signup
   );
-  const [signup, setSignup] = useLocalStorage(signupKey);
-
+  const [signup] = useLocalStorage(signupKey);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (signup) {
+      dispatch(createWorkspace(signup));
+      if (signup.confirmed) dispatch(confirmSignupEmail(signup.confirmed));
+    }
+  }, [signup]);
 
   switch (type) {
     case "WORKSPACE":
       return children;
 
     case "CONFIRMATION": {
+      if (signup || confirmed) {
+        return <Navigate to="/signup/step2" />;
+      }
+
       if (workspace && email && agreed && authorized) {
         return children;
       } else if (signup) {
-        dispatch(createWorkspace(signup));
-        return <Navigate to="/signup/email-sent" />;
+        return children;
       } else return <Navigate to="/signup/step1" />;
     }
 
     case "PROFILE":
-      return confirmed ? children : <Navigate to="/signup/email-sent" />;
-
+      if (confirmed) {
+        return children;
+      } else if (signup) {
+        return children;
+      } else return <Navigate to="/signup/email-sent" />;
     default:
-      return <Navigate to="/" />;
+      return <NotFound />;
   }
 };
 
